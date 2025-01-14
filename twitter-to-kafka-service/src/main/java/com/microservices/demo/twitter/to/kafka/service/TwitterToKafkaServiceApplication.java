4df385afef815e54eb07e8dd68ca84aa25292a36
JacekGos <jacekg93@gmail.com>
2025-01-14 19:25:02 +0100

    added TwitterToKafkaServiceConfigData configuration from application.yml

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,20 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("App started...");
     }
 }
