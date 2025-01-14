4a71ecaf1bfdab4cbd1d10f6805553769ec5b369
JacekGos <jacekg93@gmail.com>
2025-01-14 19:35:22 +0100

    reading data from config component

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,20 +1,32 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
-import jakarta.annotation.PostConstruct;
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("App started...");
+        LOG.info("App started...");
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[0])));
+        LOG.info("Welcome message: {}", twitterToKafkaServiceConfigData.getWelcomeMessage());
     }
 }
