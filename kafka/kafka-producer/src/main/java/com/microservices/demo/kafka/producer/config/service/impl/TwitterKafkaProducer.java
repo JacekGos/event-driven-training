7a2166fa3b4ee1134729a8bcbd1030ca67646e9b
JacekGos <jacekg93@gmail.com>
2025-01-19 11:20:22 +0100

    added TwitterKafkaProducer with sending message methods

---
+++ kafka/kafka-producer/src/main/java/com/microservices/demo/kafka/producer/config/service/impl/TwitterKafkaProducer.java
@@ -0,0 +1,65 @@
+package com.microservices.demo.kafka.producer.config.service.impl;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.microservices.demo.kafka.avro.model.TwitterAvroModel;
+import com.microservices.demo.kafka.producer.config.service.KafkaProducer;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PreDestroy;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+@Service
+public class TwitterKafkaProducer implements KafkaProducer<Long, TwitterAvroModel> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaProducer.class);
+
+    private final KafkaTemplate<Long, TwitterAvroModel> kafkaTemplate;
+
+    public TwitterKafkaProducer(KafkaTemplate<Long, TwitterAvroModel> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void send(String topicName, Long key, TwitterAvroModel message) {
+        LOG.info("Sending message='{}' to topic='{}'", message, topicName);
+
+        CompletableFuture<SendResult<Long, TwitterAvroModel>> kafkaResultFuture
+                = kafkaTemplate.send(topicName, key, message).completable();
+
+        addCallback(topicName, message, kafkaResultFuture);
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            LOG.info("Closing kafka producer!");
+            kafkaTemplate.destroy();
+        }
+    }
+
+    private void addCallback(String topicName, TwitterAvroModel message, CompletableFuture<SendResult<Long, TwitterAvroModel>> kafkaResultFuture) {
+        kafkaResultFuture.whenComplete(getCallback(topicName, message));
+    }
+
+    private BiConsumer<SendResult<Long, TwitterAvroModel>, Throwable> getCallback(String topicName, TwitterAvroModel message) {
+        return (result, ex) -> {
+            if (ex == null) {
+                RecordMetadata metadata = result.getRecordMetadata();
+                LOG.info("Received new metadata. Topic: {}; Partition {}; Offset {}; Timestamp {}, at time {}",
+                        metadata.topic(),
+                        metadata.partition(),
+                        metadata.offset(),
+                        metadata.timestamp(),
+                        System.nanoTime());
+            } else {
+                LOG.error("Error while sending message {} to topic {}", message.toString(), topicName, ex);
+            }
+        };
+    }
+}
