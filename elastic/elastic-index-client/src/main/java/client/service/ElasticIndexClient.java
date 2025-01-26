944bf4d45c2be2b91110e432ba4a8b80284b6183
JacekGos <jacekg93@gmail.com>
2025-01-26 11:55:22 +0100

    added elastic-index-client service, repository, utils

---
+++ elastic/elastic-index-client/src/main/java/client/service/ElasticIndexClient.java
@@ -0,0 +1,10 @@
+package client.service;
+
+
+import index.IndexModel;
+
+import java.util.List;
+
+public interface ElasticIndexClient<T extends IndexModel> {
+    List<String> save(List<T> documents);
+}
