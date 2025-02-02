7849be3ee710fd6b3abc98826e79af8a5826fa97
JacekGos <jacekg93@gmail.com>
2025-02-02 11:05:58 +0100

    Added application.yml for elastic-query-service

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/ElasticQueryServiceApplication.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/ElasticQueryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = "com.microservices.demo")
 public class ElasticQueryServiceApplication {
