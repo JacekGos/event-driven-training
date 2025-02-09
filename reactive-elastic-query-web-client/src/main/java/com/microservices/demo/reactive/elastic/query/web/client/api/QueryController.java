d2786ada63a3f4a0da992b14e1c055bb36cc367f
JacekGos <jacekg93@gmail.com>
2025-02-09 15:06:13 +0100

    Fixed reactive QueryController bug with model binding

--- reactive-elastic-query-web-client/src/main/java/com/microservices/demo/reactive/elastic/query/web/client/api/QueryController.java
+++ reactive-elastic-query-web-client/src/main/java/com/microservices/demo/reactive/elastic/query/web/client/api/QueryController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring6.context.webflux.IReactiveDataDriverContextVariable;
 import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
@@ -43,7 +44,9 @@ public class QueryController {
     }
 
     @PostMapping(value = "/query-by-text")
-    public String queryByText(@Valid ElasticQueryWebClientRequestModel requestModel, Model model) {
+    public String queryByText(
+            @Valid @ModelAttribute("elasticQueryClientRequestModel") ElasticQueryWebClientRequestModel requestModel,
+            Model model) {
         Flux<ElasticQueryWebClientResponseModel> responseModel = elasticQueryWebClient.getDataByText(requestModel);
         responseModel = responseModel.log();
         IReactiveDataDriverContextVariable reactiveData =
