dc123220e24139302d2bbf0fed636c4ec6cb2677
JacekGos <jacekg93@gmail.com>
2025-02-11 17:45:41 +0100

    Added oauth2 related classes for handling auth

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/security/TwitterQueryUserDetailsService.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/security/TwitterQueryUserDetailsService.java
@@ -8,22 +8,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class TwitterQueryUserDetailsService implements UserDetailsService {
 
-    private final QueryUserService queryUserService;
+//    private final QueryUserService queryUserService;
+//
+//    private final UserPermissionsToUserDetailTransformer userPermissionsToUserDetailTransformer;
 
-    private final UserPermissionsToUserDetailTransformer userPermissionsToUserDetailTransformer;
-
-    public TwitterQueryUserDetailsService(QueryUserService userService,
-                                          UserPermissionsToUserDetailTransformer transformer) {
-        this.queryUserService = userService;
-        this.userPermissionsToUserDetailTransformer = transformer;
-    }
+//    public TwitterQueryUserDetailsService(QueryUserService userService,
+//                                          UserPermissionsToUserDetailTransformer transformer) {
+//        this.queryUserService = userService;
+//        this.userPermissionsToUserDetailTransformer = transformer;
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return queryUserService
-                .findAllPermissionsByUsername(username)
-                .map(userPermissionsToUserDetailTransformer::getUserDetails)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException("No user found with name " + username));
+        return TwitterQueryUser.builder()
+                .username(username)
+                .build();
+//        return queryUserService
+//                .findAllPermissionsByUsername(username)
+//                .map(userPermissionsToUserDetailTransformer::getUserDetails)
+//                .orElseThrow(
+//                        () -> new UsernameNotFoundException("No user found with name " + username));
     }
 }
